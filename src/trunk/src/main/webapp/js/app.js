2350253468b3690dacd4fd03d21afb160df791a1
sgate1 <jesse.soares@lemaf.ufla.br>
2012-06-30 13:38:14 -0300

    adding files of user interfaces

---
+++ src/trunk/src/main/webapp/js/app.js
@@ -0,0 +1,15 @@
+ROOT = 'http://localhost:8080/Sgate';
+
+
+jQuery(document).ready(function() {
+	
+	// ação do menu 'Serviços'
+	$('#menu_servicos').click( function() {
+	
+		var servicoView = new ServicoView();
+		servicoView.listView();
+		
+	} );
+	
+	
+});
